c7f703ba3af223e4ddf78c6f08e0972aa1f31a29
Houseki <chidojiro@gmail.com>
2018-12-12 01:12:30 +0700

    Responsive

---
+++ build/precache-manifest.a1b718a1a5c9b6992eb2404d1b358003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a3aecce6c4dce095e30",
+    "url": "/static/js/main.3a3aecce.chunk.js"
+  },
+  {
+    "revision": "5c78d7efd9159e1bb05f",
+    "url": "/static/js/1.5c78d7ef.chunk.js"
+  },
+  {
+    "revision": "3a3aecce6c4dce095e30",
+    "url": "/static/css/main.076f59ef.chunk.css"
+  },
+  {
+    "revision": "3267de1fd54a28c3b888e781abc36069",
+    "url": "/index.html"
+  }
+];
